0873b60a25a0c287bb45e421fefdd1dcd55f4a1e
Marcin Kawa <lerukcja2002@gmail.com>
2024-10-26 18:48:04 +0200

    add ErrorPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './LoginForm/LoginForm';
 import RegisterForm from './RegisterForm/RegisterForm';
 import LandingPage from './LandingPage/LandingPage';
+import ErrorPage from './ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
