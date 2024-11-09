aa887180c8b51f52f3f9b19a583a5b7af0c72a19
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-11-09 21:04:14 +0100

    add orm for user table

---
+++ backend/src/main/java/com/coinmasters/entity/User.java
@@ -0,0 +1,39 @@
+package com.coinmasters.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = Users)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "password_salt")
+    private String passwordSalt;
+
+    @Column(name = "mail")
+    private String mail;
+
+    public User(String name, String password, String passwordSalt, String mail){
+        this.name = name;
+        this.password = password;
+        this.passwordSalt = passwordSalt;
+        this.mail = mail;
+    }
+}
