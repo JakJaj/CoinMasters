b8137b5862d50910b3db051f5d76049750af7f9b
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-11-09 21:17:34 +0100

    add orm for transaction table

---
+++ backend/src/main/java/com/coinmasters/entity/Transaction.java
@@ -0,0 +1,44 @@
+package com.coinmasters.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private Long transactionId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "date")
+    private String date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    public Transaction(String name, String category, String date, User user, Group group) {
+        this.name = name;
+        this.category = category;
+        this.date = date;
+        this.user = user;
+        this.group = group;
+    }
+}
