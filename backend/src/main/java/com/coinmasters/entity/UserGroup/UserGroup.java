d5343046f9fd28e4f8f50f4b8d2766ecd391a530
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-11-09 21:46:28 +0100

    add a intermediate table for user-group relation

---
+++ backend/src/main/java/com/coinmasters/entity/UserGroup/UserGroup.java
@@ -0,0 +1,34 @@
+package com.coinmasters.entity.UserGroup;
+
+import com.coinmasters.entity.Group;
+import com.coinmasters.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserGroup {
+
+    @EmbeddedId
+    private UserGroupId id;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @MapsId("groupId")
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Column(name = "join_date")
+    private String joinDate;
+
+}
