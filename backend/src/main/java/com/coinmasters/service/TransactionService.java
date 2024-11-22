14270061169238cec2670ed580cc6be3644e3e42
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-11-22 16:19:10 +0100

    add a method for creating a new transaction in a specified group by current user

--- backend/src/main/java/com/coinmasters/service/TransactionService.java
+++ backend/src/main/java/com/coinmasters/service/TransactionService.java
@@ -1,12 +1,19 @@
 package com.coinmasters.service;
 
 
+import com.coinmasters.config.JwtService;
 import com.coinmasters.controller.transactions.GroupTransactionResponse;
+import com.coinmasters.controller.transactions.TransactionAddRequest;
+import com.coinmasters.dao.GroupRepository;
 import com.coinmasters.dao.TransactionRepository;
+import com.coinmasters.dao.UserRepository;
 import com.coinmasters.dto.TransactionDTO;
+import com.coinmasters.entity.Group;
 import com.coinmasters.entity.Transaction;
+import com.coinmasters.entity.User;
 import com.coinmasters.exceptions.NoSuchGroupException;
 import com.coinmasters.exceptions.NoSuchTransactionException;
+import com.coinmasters.exceptions.NoSuchUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +25,9 @@ import java.util.stream.Collectors;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
-
+    private final GroupRepository groupRepository;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
 
     public GroupTransactionResponse getTransactionOfGroup(Long groupId){
         return transactionRepository.findTransactionsByGroup_GroupId(groupId)
@@ -29,6 +38,7 @@ public class TransactionService {
                                         .name(transaction.getName())
                                         .category(transaction.getCategory())
                                         .creatorName(transaction.getUser().getName())
+                                        .amount(transaction.getAmount())
                                         .date(transaction.getDate())
                                         .build()
                                 ).collect(Collectors.toList()))
@@ -47,10 +57,45 @@ public class TransactionService {
                     .category(existingTransaction.getCategory())
                     .date(existingTransaction.getDate())
                     .creatorName(existingTransaction.getUser().getName())
+                    .amount(existingTransaction.getAmount())
                     .build();
         }
         else {
             throw new NoSuchTransactionException(String.format("No transaction with id - %s", transactionID));
         }
+    }
+
+    public TransactionDTO addTransaction(TransactionAddRequest request, String token) {
+
+
+        String email = jwtService.extractUsername(token.substring(7));
+        Optional<Group> group = groupRepository.getGroupByGroupId(request.getGroupId());
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (group.isPresent() && user.isPresent()) {
+            Transaction transactionToSave = Transaction.builder()
+                    .name(request.getName())
+                    .category(request.getCategory())
+                    .date(request.getDate())
+                    .amount(request.getAmount())
+                    .group(group.get())
+                    .user(user.get())
+                    .build();
+            Transaction savedTransaction = transactionRepository.save(transactionToSave);
+            return TransactionDTO.builder()
+                    .transactionId(savedTransaction.getTransactionId())
+                    .name(savedTransaction.getName())
+                    .category(savedTransaction.getCategory())
+                    .date(savedTransaction.getDate())
+                    .creatorName(savedTransaction.getUser().getName())
+                    .amount(savedTransaction.getAmount())
+                    .build();
+
+        } else if (group.isEmpty()) {
+            throw new NoSuchGroupException(String.format("No group with id - %s", request.getGroupId()));
+        }else {
+            throw new NoSuchUserException("This should never happen. If it did then, like, damn...");
+        }
+
     }
 }
