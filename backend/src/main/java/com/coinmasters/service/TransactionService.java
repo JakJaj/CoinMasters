32cb1ceac79307552b09d85190e0724ddb2e268c
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-11-22 15:22:28 +0100

    add method for getting a transactions details and an exception when there is no transaction with a specified id

--- backend/src/main/java/com/coinmasters/service/TransactionService.java
+++ backend/src/main/java/com/coinmasters/service/TransactionService.java
@@ -4,10 +4,13 @@ package com.coinmasters.service;
 import com.coinmasters.controller.transactions.GroupTransactionResponse;
 import com.coinmasters.dao.TransactionRepository;
 import com.coinmasters.dto.TransactionDTO;
+import com.coinmasters.entity.Transaction;
 import com.coinmasters.exceptions.NoSuchGroupException;
+import com.coinmasters.exceptions.NoSuchTransactionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,5 +35,22 @@ public class TransactionService {
                         .build())
                 .orElseThrow(() -> new NoSuchGroupException(String.format("There is no group with groupID - %s", groupId.toString())));
 
+    }
+
+    public TransactionDTO getTransactionDetails(Long transactionID) {
+        Optional<Transaction> transaction = transactionRepository.findTransactionsByTransactionId(transactionID);
+        if (transaction.isPresent()){
+            Transaction existingTransaction = transaction.get();
+            return TransactionDTO.builder()
+                    .transactionId(existingTransaction.getTransactionId())
+                    .name(existingTransaction.getName())
+                    .category(existingTransaction.getCategory())
+                    .date(existingTransaction.getDate())
+                    .creatorName(existingTransaction.getUser().getName())
+                    .build();
+        }
+        else {
+            throw new NoSuchTransactionException(String.format("No transaction with id - %s", transactionID));
+        }
     }
 }
