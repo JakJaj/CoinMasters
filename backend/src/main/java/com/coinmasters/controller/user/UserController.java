ce5fe8fa704fbf0f973a504b335157c7e5b6a2b5
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-11-24 02:07:07 +0100

    add javadoc for changing password endpoing

--- backend/src/main/java/com/coinmasters/controller/user/UserController.java
+++ backend/src/main/java/com/coinmasters/controller/user/UserController.java
@@ -33,6 +33,15 @@ public class UserController {
     public ResponseEntity<UserGroupsResponse> getGroupsOfUser(@RequestHeader("Authorization") String token){
         return ResponseEntity.ok(userService.getGroupsOfUser(token));
     }
+
+    /**
+     * Endpoint for changing user's password.
+     * The Password will not be changed if the old password doesn't match the provided old password
+     * or when a new password is the same as the old one.
+     * @param request The {@link ChangePasswordRequest} schema request.
+     * @param token this token is provided in an Authorization header without (you will get it after logging in)
+     * @return The {@link ChangePasswordResponse} schema that is presenting whether a password was changes or if something went wrong.
+     */
     @PutMapping("/password")
     public ResponseEntity<ChangePasswordResponse> changePassword(@RequestBody ChangePasswordRequest request, @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(userService.changePassword(request, token));
