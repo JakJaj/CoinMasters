7bc3765b2355ee98083eb334f3f674de9369f3c4
Marcin Kawa <lerukcja2002@gmail.com>
2024-12-06 18:30:13 +0100

    add groupPage

--- frontend/src/GroupPage/GroupPage.jsx
+++ frontend/src/GroupPage/GroupPage.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GroupPage.css";
 import { Link } from "react-router-dom";
 
-
 const GroupPage = () => {
+    const [groups, setGroups] = useState([]);
+
+    const addGroup = () => {
+        const newId = Date.now().toString();
+        setGroups([...groups, { id: newId, name: "Nowa Grupa" }]);
+    };
+
     return (
         <div className="group-wrapper">
-            <div className="group">
-                <div className="group-id">178951</div>
-                <div className="group-name">Rachuneczki</div>
-                <Link to="/dashboard">
-                    <button className="group-btn">SELECT</button>
-                </Link>
+            {groups.map((group, index) => (
+                <div className="group" key={index}>
+                    <div className="group-id">{group.id}</div>
+                    <div className="group-name">{group.name}</div>
+                    <Link to="/dashboard">
+                        <button className="group-btn">SELECT</button>
+                    </Link>
+                </div>
+            ))}
+            <div className="add-card" onClick={addGroup}>
+                +
             </div>
         </div>
     );
-}
+};
 
 export default GroupPage;
