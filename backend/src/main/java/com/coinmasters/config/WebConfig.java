c7713c1c43109e49816d1937c5b0b670af24a572
Jakub Jajonek <jakub.jajonek@gmail.com>
2025-01-23 07:47:56 +0100

    new ip handeling

--- backend/src/main/java/com/coinmasters/config/WebConfig.java
+++ backend/src/main/java/com/coinmasters/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://main.d24nz4wd5e22qf.amplifyapp.com")
+                .allowedOrigins("http://localhost:5173", "http://3.75.84.102")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
